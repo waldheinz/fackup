d7d3d516d30376cffcb6857caf81fe950cd25a75
Matthias Treydte <waldheinz@gmail.com>
2012-01-24 13:50:08 +0100

    Re-implemented authorization.
    Forgot to add the file with the last commit...

--- src/Main.hs
+++ src/Main.hs
@@ -10,20 +10,25 @@ import Network.OAuth.Http.Request
 import Network.OAuth.Http.Response
 
 reqUrl   = fromJust $ parseURL "http://www.flickr.com/services/oauth/request_token"
-accUrl   = fromJust $ parseURL "https://api.login.yahoo.com/oauth/v2/get_token"
+accUrl   = fromJust $ parseURL "http://www.flickr.com/services/oauth/access_token"
 srvUrl   = fromJust $ parseURL "http://query.yahooapis.com/v1/yql?q=select%20%2A%20from%20social.profile%20where%20guid%3Dme"
-authUrl  = head . find (=="xoauth_request_auth_url") . oauthParams
-app      = Application "<consumer key>" "<consumer secret>" OOB
+app      = Application "eeb2b5a4cde6988dec44195952ce1cbe" "fdc50914cdbc3bd8" OOB
 token    = fromApplication app
 
 response = runOAuthM token $ do
-   signRq2 PLAINTEXT Nothing reqUrl >>= oauthRequest CurlClient
- --  cliAskAuthorization authUrl
- --  signRq2 PLAINTEXT Nothing accUrl >>= oauthRequest CurlClient
- --  signRq2 HMACSHA1 (Just $ Realm "yahooapis.com") srvUrl >>= serviceRequest CurlClient
-   
+   signRq2 HMACSHA1 Nothing reqUrl >>= oauthRequest CurlClient
+   cliAskAuthorization $ \t -> "http://www.flickr.com/services/oauth/authorize?oauth_token=" ++ 
+                       (head . find (=="oauth_token") . oauthParams) t
+   signRq2 HMACSHA1 Nothing accUrl >>= oauthRequest CurlClient
+   t <- getToken
+   let
+        secret = head . find (== "oauth_token_secret") . oauthParams
+        token = head . find (== "oauth_token") . oauthParams
+   return (token t, secret t)
+
+storedAuth = ("72157628981807463-1c78795ce3b82eb0","5b71c229f5d640b5")
+
 main :: IO ()
 main = do
    r <- response
-   return ()
---   print r
+   print r
