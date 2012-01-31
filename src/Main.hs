54eaad17f787fd0eda3cf0a256654d9a93719418
Matthias Treydte <waldheinz@gmail.com>
2012-01-31 12:15:17 +0100

    Partial photo list query.

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,7 @@ module Main ( main ) where
 import Debug.Trace
 
 import Control.Applicative ((<*>))
-import Control.Monad (mzero)
+import Control.Monad (forM_, mzero)
 import Data.Aeson
 import Data.Aeson.Types (Parser, parseMaybe, Value)
 import Data.Attoparsec.ByteString.Lazy (parseOnly)
@@ -44,17 +44,28 @@ authToken :: (String, String) -> Token
 authToken (t, s) = AccessToken app fs where
    fs = insert ("oauth_token_secret", s) $ singleton ("oauth_token", t)
 
-callFlickr :: Token -> String -> IO Response
-callFlickr t m = runOAuthM t $ signRq2 HMACSHA1 Nothing url >>= serviceRequest CurlClient where
-   url =  srvUrl { qString = insert ("method", m) $ qString srvUrl }
+callFlickr
+   :: Token       -- ^ auth token
+   -> String      -- ^ method name
+   -> FieldList   -- ^ additional parameters
+   -> (Value -> Parser a)
+   -> IO a
+callFlickr t m ps p = runOAuthM t $ do
+   req <- signRq2 HMACSHA1 Nothing $
+          srvUrl { qString = union ps $ insert ("method", m) $ qString srvUrl }
+   res <- parseOnly json . unlazy . rspPayload <$> serviceRequest CurlClient req
+   return $ fromMaybe (error "cannot parse Flickr response") $
+            parseMaybe p $ either error id res
+   where
+      unlazy = BS.concat . toChunks
 
----------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 -- Photo Sets
----------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 data PhotoSet = PhotoSet 
    { psId     :: String
-   , name     :: String
+   , psName   :: String
    } deriving (Show)
 
 instance FromJSON PhotoSet where
@@ -62,24 +73,46 @@ instance FromJSON PhotoSet where
                              v .: "id" <*>
                              (v .: "title" >>= \x -> x .: "_content")
    parseJSON _ = mzero
-   
-unlazy :: Data.ByteString.Lazy.ByteString -> BS.ByteString
-unlazy = BS.concat . toChunks
 
 getSets :: Token -> IO [PhotoSet]
-getSets t = do
-   r <- parseOnly json . unlazy . rspPayload <$> callFlickr t "flickr.photosets.getList"
-   return $ fromMaybe (error "Invalid response from Flickr") $
-            parseMaybe parseSets $ either error id r
-   where
-      parseSets :: Value -> Data.Aeson.Types.Parser [PhotoSet]
-      parseSets (Object o) = do
-         (Array a) <- o .: "photosets" >>= \x -> x .: "photoset"
-         mapM parseJSON $ V.toList a
-      parseSets _ = mzero
+getSets t = callFlickr t "flickr.photosets.getList" empty parseSets where
+   parseSets :: Value -> Data.Aeson.Types.Parser [PhotoSet]
+   parseSets (Object o) = do
+      (Array a) <- o .: "photosets" >>= \x -> x .: "photoset"
+      mapM parseJSON $ V.toList a
+   parseSets _ = mzero
 
+--------------------------------------------------------------------------------
+-- Individual Photos
+--------------------------------------------------------------------------------
+
+data Photo = Photo
+   { pId    :: String
+   , title  :: String
+   } deriving (Show)
+
+-- | gets one page of photos from a set
+getPhotos' :: Token -> PhotoSet -> Int -> IO [Photo]
+getPhotos' t s p = callFlickr t
+                  "flickr.photosets.getPhotos"
+                  (insert ("page", show p) $ singleton ("photoset_id", psId s))
+                  pp where
+                     
+   pp :: Value -> Data.Aeson.Types.Parser [Photo]
+   pp = undefined
+
+getPhotos :: Token -> PhotoSet -> IO [Photo]
+getPhotos t s = undefined
+   
 main :: IO ()
 main = do
    let t = authToken storedAuth
-   r <- getSets t
-   print r
+   putStrLn "getting photo sets..."
+   sets <- getSets t
+   putStrLn $ "found " ++ (show $ length sets) ++ " sets"
+   
+   forM_ sets $ \s -> do
+      putStrLn $ "processing set \"" ++ (psName s) ++ "\" (" ++ psId s ++ ") ..."
+      ps <- getPhotos t s
+      putStrLn $ "   contains " ++ (show $ length ps) ++ " photos"
+      
